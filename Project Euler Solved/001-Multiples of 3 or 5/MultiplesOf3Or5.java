02d16f6707b14c27da350a070582719c10630cfa
Ahmet Baş <ahba8184@gmail.com>
2023-03-25 00:06:22 +0300

    Update MultiplesOf3Or5.java

--- Project Euler Solved/001-Multiples of 3 or 5/MultiplesOf3Or5.java
+++ Project Euler Solved/001-Multiples of 3 or 5/MultiplesOf3Or5.java
@@ -2,7 +2,6 @@ public class MultiplesOf3Or5 {
     public static void main(String[] args) {
         multiples(1000);
     }
-
     public static void multiples(int number){
         int sum = 0;
         for (int i = 1; i < number; i++) {
@@ -10,13 +9,6 @@ public class MultiplesOf3Or5 {
                 sum = sum + i;
             }
         }
-        System.out.println(number + " sayısından küçük 3 veya 5 ile\n" +
-                "bölünebilen sayıların toplamı : " + sum);
+        System.out.println(number + " sayısından küçük 3 veya 5 ile\nbölünebilen sayıların toplamı : " + sum);
     }
-
-
-
-
-
-
 }
