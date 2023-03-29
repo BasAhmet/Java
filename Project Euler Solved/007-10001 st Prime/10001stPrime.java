a4fdc2081673315f398b9b7da567a492212f914c
Ahmet Baş <ahba8184@gmail.com>
2023-03-29 17:06:17 +0300

    Update 10001stPrime.java

--- Project Euler Solved/007-10001 st Prime/10001stPrime.java
+++ Project Euler Solved/007-10001 st Prime/10001stPrime.java
@@ -1,20 +1,4 @@
-package practices.projecteulernetproblem;
-
-public class P007_10001stPrime {
-    /*
-    10001st prime
-    Problem 7
-    By listing the first six prime numbers:
-    2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-    What is the 10 001st prime number?
-     */
-    /*
-    10001. asal sayı
-    Soru : 7
-    İlk altı asal sayıyı listeleyerek:
-    2, 3, 5, 7, 11 ve 13, 6. asalın 13 olduğunu görebiliriz.
-    10 001'inci asal sayı nedir?
-     */
+public class 10001stPrime {
     //104743
     public static void main(String[] args) {
         int primeNumber = 0;
