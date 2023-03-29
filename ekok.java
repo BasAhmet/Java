d6ff1992768f065d77a52d974e78c10552ff09fe
Ahmet Baş <ahba8184@gmail.com>
2023-03-29 16:18:05 +0300

    Update and rename ekok to ekok.java

---
+++ ekok.java
@@ -0,0 +1,16 @@
+public static int ekok(int number1, int number2){
+    int carpan1 = 2;
+    int carpan2 = 2;
+    while(number1 != number2){
+        if(number1<number2){
+            number1 /= carpan1-1;
+            number1 *= carpan1;
+            carpan1++;
+        }else if(number1>number2){
+            number2 /= carpan2-1;
+            number2 *= carpan2;
+            carpan2++;
+        }
+    }
+    return number1;
+}
