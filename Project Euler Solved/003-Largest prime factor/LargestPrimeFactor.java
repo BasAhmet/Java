90a0ba7e4431b485e503a7be94a0272f4ae1aebe
Ahmet Baş <ahba8184@gmail.com>
2023-03-29 15:40:49 +0300

    Update LargestPrimeFactor.java

--- Project Euler Solved/003-Largest prime factor/LargestPrimeFactor.java
+++ Project Euler Solved/003-Largest prime factor/LargestPrimeFactor.java
@@ -1,22 +1,7 @@
-package practices.projecteulernetproblem;
-
-public class P003_LargestPrimeFactor {
+public class LargestPrimeFactor {
     public static void main(String[] args) {
         long number = 600851475143L;
         largestPrimeFactor(number);
-    }
-    /*
-    Largest prime factor
-    Problem 3
-    The prime factors of 13195 are 5, 7, 13 and 29.
-    What is the largest prime factor of the number 600851475143 ?
-    */
-    /*
-    En büyük asal çarpan
-    Soru : 3
-    13195'in asal çarpanları 5, 7, 13 ve 29'dur.
-    600851475143 sayısının en büyük asal çarpanı nedir?
-    */
     //71, 839, 1471, 6857
     public static long isItPrime(long number) {
         int numberSqrt = (int) Math.sqrt(number);
@@ -33,6 +18,7 @@ public class P003_LargestPrimeFactor {
         }
         return result;
     }
+        
     public static void largestPrimeFactor(long number){
         long section;
         for (long i = 2; i < number ; i++) {
