2914c4a1308e7d0cb1c0cc1bd197b701693dde87
Ahmet Baş <ahba8184@gmail.com>
2023-03-29 16:14:46 +0300

    Update SmallestMultiple.java

--- Project Euler Solved/005-Smallest multiple/SmallestMultiple.java
+++ Project Euler Solved/005-Smallest multiple/SmallestMultiple.java
@@ -1,18 +1,4 @@
-package practices.projecteulernetproblem;
-
-public class P005_SmallestMultiple {
-    /*
-    Smallest multiple
-    Problem 5
-    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-    */
-    /*
-    En küçük çoklu
-    Soru : 5
-    2520, 1'den 10'a kadar olan sayıların her birine kalansız bölünebilen en küçük sayıdır.
-    1'den 20'ye kadar tüm sayılara bölünebilen en küçük pozitif sayı nedir?
-    */
+public class SmallestMultiple {
     public static void main(String[] args) {
         int result = 1;
         for (int i = 1; i < 21 ; i++) {
