a963e1a4fceac787ffba1a48583e1d3128317c51
Ahmet Baş <ahba8184@gmail.com>
2023-03-30 14:03:32 +0300

    Update LargestPrimeFactor.java

--- Project Euler Solved/003-Largest prime factor/LargestPrimeFactor.java
+++ Project Euler Solved/003-Largest prime factor/LargestPrimeFactor.java
@@ -1,11 +1,7 @@
+//71, 839, 1471, 6857
 public class LargestPrimeFactor {
-    public static void main(String[] args) {
-        long number = 600851475143L;
-        largestPrimeFactor(number);
-    //71, 839, 1471, 6857
-    public static long isItPrime(long number) {
+    public static boolean isItPrime(long number) {
         int numberSqrt = (int) Math.sqrt(number);
-        long result = 0;
         boolean isItPrime = true;
         for (int i = 2; i < numberSqrt + 1; i++) {
             if (number % i == 0) {
@@ -13,20 +9,15 @@ public class LargestPrimeFactor {
                 break;
             }
         }
-        if (isItPrime) {
-            result = number;
-        }
-        return result;
+        return isItPrime;
     }
-        
     public static void largestPrimeFactor(long number){
         long section;
         for (long i = 2; i < number ; i++) {
             if (number % i == 0) {
                 section = number/i;
-                long result = isItPrime(section);
-                if (result != 0) {
-                    System.out.println(result);
+                if (isItPrime(section)) {
+                    System.out.println(section);
                     break;
                 }
             }
